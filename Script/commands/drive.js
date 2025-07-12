c348b435726f68fec1d61f4b96ed77713569dbea
RAJA ViP 5X <162680957+rajaproject79@users.noreply.github.com>
2025-07-12 07:07:19 +0600

    Update drive.js

--- Script/commands/drive.js
+++ Script/commands/drive.js
@@ -28,7 +28,7 @@ module.exports.run = async function({ api, event, args }) {
  }
 
  try {
- const apiURL = `https://web-api-delta.vercel.app/drive?url=${encodeURIComponent(inputUrl)}`;
+ const apiURL = `https://aryan-xyz-google-drive.vercel.app/drive?url=${encodeURIComponent(inputUrl)}`;
  const res = await axios.get(apiURL);
 
  const data = res.data || {};
