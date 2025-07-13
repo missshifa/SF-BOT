0219a4a74d0ec88847ec46504c00d623c4645022
RAJA ViP 5X <162680957+rajaproject79@users.noreply.github.com>
2025-07-13 08:51:15 +0600

    Create Raja01.js

---
+++ Script/commands/Raja01.js
@@ -0,0 +1,124 @@
+module.exports.config = {
+  name: ",Raja01",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "RAJA ViP 5X",
+  description: "sand best islamick Video",
+  commandCategory: "noprefix",
+  usages: "😒",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+
+};
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+    if (event.body.indexOf("aso")==0 || (event.body.indexOf("janina")==0) || event.body.indexOf("ami")==0 ||
+event.body.indexOf("Dhur")==0 ||
+event.body.indexOf("oi")==0 ||
+event.body.indexOf("Suna")==0 ||
+event.body.indexOf("Jan")==0 ||
+event.body.indexOf("Kemon")==0 ||
+event.body.indexOf("oi jan")==0 ||
+event.body.indexOf("Ki")==0) {
+    const axios = global.nodemodule["😅"];
+const request = global.nodemodule["🥲"];
+const fs = global.nodemodule["fs-extra"];
+    var link = [ "https://i.imgur.com/JxSYk0b.mp4", "https://i.imgur.com/6LHAb4R.mp4",
+"https://i.imgur.com/Vnuam22.mp4",
+"https://i.imgur.com/h1TEgbn.mp4",
+"https://i.imgur.com/iC5t8Xd.mp4",
+"https://i.imgur.com/jRCtMZM.mp4",
+"https://i.imgur.com/flgABWM.mp4",
+"https://i.imgur.com/fTobCMI.mp4",
+"https://i.imgur.com/4nWaJed.mp4",
+"https://i.imgur.com/r2bzwnt.mp4",
+"https://i.imgur.com/m1WJX4L.mp4",
+"https://i.imgur.com/U3vUCU4.mp4",
+"https://i.imgur.com/eCy6iyu.mp4",
+"https://i.imgur.com/i0vXz4A.mp4",
+"https://i.imgur.com/BNIR463.mp4",
+"https://i.imgur.com/azfa064.mp4",
+"https://i.imgur.com/NVkqTV3.mp4",
+"https://i.imgur.com/oP4RHOZ.mp4",
+"https://i.imgur.com/oTgxKfg.mp4",
+"https://i.imgur.com/efpHAn8.mp4",
+",https://i.imgur.com/MLtfe0L.mp4",
+"https://i.imgur.com/s1HpoDU.mp4",
+"https://i.imgur.com/X3FdGKj.mp4",
+"https://i.imgur.com/cxxJ9mK.mp4",
+"https://i.imgur.com/Yn4aJO2.mp4",
+"https://i.imgur.com/qKX9s2j.mp4",
+"https://i.imgur.com/Vnuam22.mp4",
+"https://i.imgur.com/h1TEgbn.mp4",
+"https://i.imgur.com/iC5t8Xd.mp4",
+"https://i.imgur.com/jRCtMZM.mp4",
+"https://i.imgur.com/flgABWM.mp4",
+"https://i.imgur.com/fTobCMI.mp4",
+"https://i.imgur.com/4nWaJed.mp4",
+"https://i.imgur.com/r2bzwnt.mp4",
+"https://i.imgur.com/m1WJX4L.mp4",
+"https://i.imgur.com/U3vUCU4.mp4",
+"https://i.imgur.com/eCy6iyu.mp4",
+"https://i.imgur.com/i0vXz4A.mp4",
+"https://i.imgur.com/BNIR463.mp4",
+"https://i.imgur.com/azfa064.mp4",
+"https://i.imgur.com/NVkqTV3.mp4",
+"https://i.imgur.com/oP4RHOZ.mp4",
+"https://i.imgur.com/oTgxKfg.mp4",
+"https://i.imgur.com/efpHAn8.mp4",
+"https://i.imgur.com/MLtfe0L.mp4",
+"https://i.imgur.com/s1HpoDU.mp4",
+"https://i.imgur.com/X3FdGKj.mp4",
+"https://i.imgur.com/cxxJ9mK.mp4",
+"https://i.imgur.com/Yn4aJO2.mp4",
+"https://i.imgur.com/qKX9s2j.mp4",];
+     var callback = () => api.sendMessage({body:`•—»✨[ 𝐏𝐫𝐞𝐟𝐢𝐱 𝐄𝐯𝐞𝐧𝐭 ]✨«—•\n•┄┅════❁🌺❁════┅┄•\n\n❂\n            🫶💜🪽
+\n___কষ্ট আর কষ্ট এতো কষ্ট আর আমার মনে রাখার জায়গা নেই জানিনা কখন হার্ট স্ট্রোক করে মারা যায় ♡🩷🕌\n\n#RAJA 卝 চৌধুরীヅ\n\n•┄┅════❁🌺❁════┅┄•\n•—»✨[ 𝐑𝐚𝐣𝐚 𝐏𝐫𝐨𝐣𝐞𝐜𝐭 ]✨«—•\n[🐰]  𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${PREFIX} ]\n[🫰] 𝐍𝐎𝐏𝐑𝐄𝐅𝐈𝐗  : 😒\n[⌛]  𝐃𝐚𝐭𝐞  : ${Date.now() - timeStart} 𝐓𝐢𝐦𝐞\n[🍒]  ${global.config.BOTNAME}`,attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+  const timeStart = Date.now();
+  const dcm = process.uptime(); 
+ var anh = Math.floor(dcm / (60 * 60));
+  var la = Math.floor((dcm % (60 * 60)) / 60);
+  var vt = Math.floor(dcm % 60);
+const res = await
+axios.get(`https://noobs-api-sable.vercel.app/text`);
+var shaon = res.data.data;
+let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+      const PREFIX = config.PREFIX;
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/2024.mp4")).on("close",() => callback());
+}
+
+  module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+    }
+  module.exports.run = async ({ event, api, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["😒"] == "undefined" || data["😒"] == true) data["😒"] = false;
+  else data["😒"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+api.sendMessage(`${(data["😒"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+}      
+
+
+
+
+
+    }
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+
+   };
